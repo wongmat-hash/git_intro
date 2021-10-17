907223a3dbc99e7f3800e4298cc62461b7b734c3
Matthew Wong <wongmat@oregonstate.edu>
2021-10-17 13:54:02 -0700

    added all the correct comments

--- CS362/BlackBox Testing/tests.py
+++ CS362/BlackBox Testing/tests.py
@@ -1,32 +1,39 @@
 import unittest
 from credit_card_validator import credit_card_validator
 
-#class test case for my 6 bug reveals
+
+# class test case for my 6 bug reveals
 class TestCase(unittest.TestCase):
 
-    #validates error case if there is no input given
+    # validates error case if there is no input given invalid check returns F
+    # picked using category partition testing
     def testBugOne(self):
         self.assertEqual(credit_card_validator(''), True)
 
-    #validates Visa cards checks if given a prefix 4 and only 15 num in length for valid length
+    # validates Visa cards checks for corect prefix and length invalid = F
+    # picked using category partition testing
     def testBugTwo(self):
         self.assertEqual(credit_card_validator('452100934314023'), True)
 
-    #validates Visa cards checks if given prefix 4 and checks length for 16
+    # checks against Mastercards checks for outside prefixs and length of 16
+    # picked using category partition testing
     def testBugThree(self):
-        self.assertEqual(credit_card_validator('4521009704314023'), True)
+        self.assertEqual(credit_card_validator('2720995085393011'), True)
 
-    #validates Amex cards checks the prefix 34 or 37 and correct input length of 16
+    # validates Amex cards checks the prefix 34 or 37 and correct length of 16
+    # picked using category partition testing 
     def testBugFour(self):
         self.assertEqual(credit_card_validator('370600982683046'), True)
 
-    #validates Amex cards checks the prefix 34 or 37 and checks for incorrect input length of 15
+    # validates Amex cards checks the prefix and for incorrect length of 15
+    # picked using category partition testing
     def testBugFive(self):
         self.assertEqual(credit_card_validator('3496626767003111'), True)
 
-    #checks for Mastercard prefix 55 and checks the length correct of 16
+    # validates Visa cards checks if given prefix 4 and checks length for 16
+    # picked using category partition testing
     def testBugSix(self):
-        self.assertEqual(credit_card_validator('5559973959023989'), True)
+        self.assertEqual(credit_card_validator('4521009704314023'), True)
 
 
 if __name__ == '__main__':

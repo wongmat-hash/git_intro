c880682a01d7a9c004768183ed9de965987c8903
Matthew Wong <wongmat@oregonstate.edu>
2021-10-25 15:45:28 -0700

    updated some data

--- CS362/White box testing/tests.py
+++ CS362/White box testing/tests.py
@@ -6,7 +6,7 @@ from contrived_func import contrived_func
 class TestCase(unittest.TestCase):
 
     # asserts equal to the contrived function with true passing in
-    # x is declaration variable we can set to anything 
+    # x is declaration variable we can set to anything
     def test1(self):
         x = 5
         self.assertEqual(contrived_func(x), 1)
@@ -16,10 +16,14 @@ class TestCase(unittest.TestCase):
         self.assertEqual(contrived_func(x), 1)
 
     def test3(self):
-        x = 6
+        x = 555
         self.assertEqual(contrived_func(x), 1)
 
     def test4(self):
+        x = 6
+        self.assertEqual(contrived_func(x), 0)
+
+    def test5(self):
         x = 161
         self.assertEqual(contrived_func(x), 0)
 

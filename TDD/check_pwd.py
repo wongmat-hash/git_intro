e3f1578b176b9158aebb479ee3c323aa995efe74
Matthew Wong <wongmat@oregonstate.edu>
2022-10-31 21:33:25 -0700

    File skeletons are all set up for A2: TDD Hands On

--- TDD/check_pwd.py
+++ TDD/check_pwd.py
@@ -1,5 +1,23 @@
 # this needs to contain the password checking function
-import unittest
+# this will contain the tests you wrote during the TDD process with main function
+# notes
+# Sample Workflow
 
-if __name__ == "__main__":
-    unittest.main()
+# Read specification
+# Write first test and run it to ensure it fails
+# Commit changes, "Wrote test1 to see if an empty string is rejected correctly"
+# Write the bare minimum implementation to get the test to pass
+# Run test to see if it passes
+# Commit changes, "Made check_pwd return False when the length is 0, it passed"
+# Write second test...
+
+def check_pwd(string):
+    return False
+    return True
+
+# requirements for True 
+# Must be between 8 and 20 characters (inclusive)
+# Must contain at least one lowercase letter
+# Must contain at least one uppercase letter
+# Must contain at least one digit
+# Must contain at least one symbol from: ~`!@#$%^&*()_+-= (copy and paste to avoid missing characters) These are the only permitted symbols
--- TDD/tests.py
+++ TDD/tests.py
@@ -1,5 +1,30 @@
-# this will contain the tests you wrote during the TDD process with main function 
+# this will contain the tests you wrote during the TDD process with main function
+# notes
+# Sample Workflow
+
+# Read specification
+# Write first test and run it to ensure it fails
+# Commit changes, "Wrote test1 to see if an empty string is rejected correctly"
+# Write the bare minimum implementation to get the test to pass
+# Run test to see if it passes
+# Commit changes, "Made check_pwd return False when the length is 0, it passed"
+# Write second test...
+
 import unittest
+from check_pwd import check_pwd
+
+
+class TestCase(unittest.TestCase):
+
+    def test_one(self):
+
 
 if __name__ == "__main__":
     unittest.main()
+
+# requirements
+# Must be between 8 and 20 characters (inclusive)
+# Must contain at least one lowercase letter
+# Must contain at least one uppercase letter
+# Must contain at least one digit
+# Must contain at least one symbol from: ~`!@#$%^&*()_+-= (copy and paste to avoid missing characters) These are the only permitted symbols
